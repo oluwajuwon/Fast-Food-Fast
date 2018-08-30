84047827a14e97e4f43dc7e4563bf645d8855891
oluwajuwon <jfagbohungbe@gmail.com>
2018-08-30 23:32:26 +0100

    Create HTML UI Templates
    Add HTML templates for both user and admin pages

---
+++ UI/assets/js/script.js
@@ -0,0 +1,17 @@
+
+/* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon */
+
+const myFunction =() =>{
+    const x = document.getElementById("myNav");
+
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } 
+    else {
+        x.className = "navbar";
+    }
+}
+
+const redirectTo = location => {
+    window.location.href = location;
+  }
