22e7d03352f3a0170874dc6fb396f8d9b6e319f7
oluwajuwon <jfagbohungbe@gmail.com>
2018-09-04 22:51:35 +0100

    Create fetch specific order API endpoint
    Create route and controller to get specific order via orderId parameter

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ import orderController from '../controllers/orders';
 
 const router = express.Router();
 router.get('/api/v1/orders', orderController.getAllorders);
+router.get('/api/v1/orders/:id', orderController.getOrder);
 
 export default router;
