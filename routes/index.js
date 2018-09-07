09fb519fcd2c312311e27e2eddbac9f7df7e7e44
oluwajuwon <jfagbohungbe@gmail.com>
2018-09-07 15:39:38 +0100

    Update API routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import orderController from '../controllers/orders';
 
 const router = express.Router();
+router.get('/', (req, res) => {
+  res.send({ hello: 'Welcome to Fast-Food-Fast API endpoint by Oluwajuwon Fagbohungbe, Please enjoy!' });
+});
 router.get('/api/v1/orders', orderController.getAllorders);
 router.get('/api/v1/orders/:id', orderController.getOrder);
 router.post('/api/v1/orders', orderController.createOrder);
