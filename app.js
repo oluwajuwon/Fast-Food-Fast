0fab2ff4d606137962758a968fcb0ca211780214
oluwajuwon <jfagbohungbe@gmail.com>
2018-09-07 15:25:52 +0100

    Create heroku config file

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import bodyParser from 'body-parser';
 import router from './routes/index';
 //  setting up the express application
 const app = express();
-const port = 3000;
+// const port = 3000;
+const PORT = process.env.PORT;
 //  parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
-app.listen(port, () => {
+app.listen(PORT);
+/*  app.listen(port, () => {
   console.log(`Server running on ${port}`);
-});
+}); */
 export default app;
