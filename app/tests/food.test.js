d2b1b6dcced25b05cc269dc7f7eadeb06aa8ab1d
oluwajuwon <jfagbohungbe@gmail.com>
2018-09-20 13:20:01 +0100

    Create test suite for adding new food to the database (Fail)

--- app/tests/food.test.js
+++ app/tests/food.test.js
@@ -3,6 +3,13 @@ import app from '../app';
 
 const request = require('supertest');
 
+const foodName = 'Chicken and chips';
+const price = '4000';
+const categoryId = '2';
+const description = 'Chicken and chips is the way forward';
+const image = 'https://cdn.pixabay.com/photo/2017/03/10/13/49/fast-food-2132863__340.jpg';
+
+
 // run API test
 describe('Test suite for Food API endpoints', () => {
   it('GET /api/v1/food should return status 200 if route is working fine', (done) => {
@@ -58,6 +65,55 @@ describe('Test suite for Food API endpoints', () => {
           expect(response.body).to.be.an('object');
           expect(response.body).to.be.an('object').with.property('db');
           expect(response.body).to.be.an('object').with.property('db').to.be.an('array');
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/v1/food/', () => {
+    it('should return status code 201 if new food was added succesfully', (done) => {
+      request(app)
+        .post('/api/v1/food')
+        .send({
+          foodName, price, categoryId, description, image,
+        })
+        .end((err, response) => {
+          expect(response.status).to.equal(201);
+          done();
+        });
+    });
+
+    it('should return status code 400 if the fields were left empty', (done) => {
+      request(app)
+        .post('/api/v1/food')
+        .send({})
+        .end((err, response) => {
+          expect(response.status).to.equal(400);
+          done();
+        });
+    });
+
+    it('should add new food item', (done) => {
+      request(app)
+        .post('/api/v1/food')
+        .send({
+          foodName, price, categoryId, description, image,
+        })
+        .end((err, response) => {
+          expect(response.body).to.be.an('object').with.property('newFood');
+          expect(response.body).to.be.an('object').with.property('newFood').to.be.an('object');
+          done();
+        });
+    });
+
+    it('should return a message saying a new food was added succesfully', (done) => {
+      request(app)
+        .post('/api/v1/food')
+        .send({
+          userId, foodId, amount, quantity,
+        })
+        .end((err, response) => {
+          expect(response.body.message).to.equal('New food has been added successfully');
           done();
         });
     });
