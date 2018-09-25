e3c309ab33362bb8855779a590925a13c71db41b
oluwajuwon <jfagbohungbe@gmail.com>
2018-09-25 14:31:04 +0100

    Change double quotes to single quotes in javascript files

--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -2,17 +2,17 @@
 /* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon */
 
 const myFunction =() =>{
-    const x = document.getElementById("myNav");
+  const nav = document.getElementById('myNav');
 
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } 
-    else {
-        x.className = "navbar";
-    }
+  if (nav.className === 'navbar') {
+    nav.className += ' responsive';
+  } 
+  else {
+    nav.className = 'navbar';
+  }
 };
 
 const redirectTo = location => {
-    window.location.href = location;
-  };
+  window.location.href = location;
+};
 
