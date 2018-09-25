7f41255e7f2c092ac07d7a8eae6c522dd8b960ad
oluwajuwon <jfagbohungbe@gmail.com>
2018-09-25 23:43:57 +0100

    Update index.js to include understandable route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
 const router = express.Router();
-router.get('/', (request, response) => {
-  response.json({ hello: 'Welcome to Fast-Food-Fast API endpoints by Oluwajuwon Fagbohungbe, Please enjoy!' });
+router.get('/api/v1', (request, response) => {
+  response.json({ hello: 'Welcome to Fast-Food-Fast API endpoints by Oluwajuwon Fagbohungbe, Please enjoy and give feedback!' });
 });
 
 export default router;
