cb530c438d76f9a48fcd4df883d0d937722d0852
oluwajuwon <jfagbohungbe@gmail.com>
2018-09-26 21:58:35 +0100

    Remove unused code

--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -1,24 +1,18 @@
 //  Get and initialize navbar icon
 const navIcon = document.getElementById('nav-icon');
 
-//  On click event listener that triggers nav toggle
-navIcon.onclick = () => {
-  navToggle();
-}
-
-//  Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon
-const navToggle =() =>{
+//  Toggle between adding and removing the "responsive" class to navbar when user clicks on the icon
+const navToggle = () => {
   const nav = document.getElementById('myNav');
 
   if (nav.className === 'navbar') {
     nav.className += ' responsive';
-  } 
-  else {
+  } else {
     nav.className = 'navbar';
   }
 };
 
-const redirectTo = location => {
-  window.location.href = location;
+//  On click event listener that triggers nav toggle
+navIcon.onclick = () => {
+  navToggle();
 };
-
